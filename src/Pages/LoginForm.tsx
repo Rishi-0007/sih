e7d7328c7fe48a7441936a34d437e7cd70141650
Rishi-0007 <rishikumarnayak9@gmail.com>
2024-09-12 17:20:48 +0530

    Refactor LoginForm component and improved code format

--- src/Pages/LoginForm.tsx
+++ src/Pages/LoginForm.tsx
@@ -1,8 +1,17 @@
-import { Box, FormControl, FormLabel, Input } from "@chakra-ui/react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Image,
+  Input,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { useNavigate } from "react-router-dom";
 import { z } from "zod";
+import loginLogo from "../assets/login.png";
 
 const schema = z.object({
   name: z.string().min(3, "Username must be at least 3 characters"),
@@ -29,45 +38,57 @@ const LoginForm = () => {
   };
 
   return (
-    <Box
-      className="container mt-5"
-      maxW="sm"
-      p={5}
-      boxShadow="md"
-      borderRadius="md"
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <FormControl className="mb-3">
-          <FormLabel htmlFor="username">Username</FormLabel>
-          <Input
-            id="username"
-            type="text"
-            placeholder="Enter your username"
-            className="form-control"
-            {...register("name")}
-          />
-          {errors.name && <p className="text-danger">{errors.name.message}</p>}
-        </FormControl>
+    <VStack spacing={4} align="center" justify="center" height="100vh">
+      <Image
+        src={loginLogo}
+        alt="Login"
+        boxSize={250}
+        objectFit="cover"
+        padding={5}
+      />
+      <Text fontSize={"4xl"}>Welcome 🙏</Text>
+      <Box
+        className="container mt-5"
+        maxW="sm"
+        p={5}
+        boxShadow="lg"
+        borderRadius="md"
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl className="mb-3">
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input
+              id="username"
+              type="text"
+              placeholder="Enter your username"
+              className="form-control"
+              {...register("name")}
+            />
+            {errors.name && (
+              <p className="text-danger">{errors.name.message}</p>
+            )}
+          </FormControl>
 
-        <FormControl className="mb-3">
-          <FormLabel htmlFor="password">Password</FormLabel>
-          <Input
-            id="password"
-            type="password"
-            placeholder="Enter your password"
-            className="form-control"
-            {...register("password")}
-          />
-          {errors.password && (
-            <p className="text-danger">{errors.password.message}</p>
-          )}
-        </FormControl>
+          <FormControl className="mb-3">
+            <FormLabel htmlFor="password">Password</FormLabel>
+            <Input
+              id="password"
+              type="password"
+              placeholder="Enter your password"
+              className="form-control"
+              {...register("password")}
+            />
+            {errors.password && (
+              <p className="text-danger">{errors.password.message}</p>
+            )}
+          </FormControl>
 
-        <button type="submit" className="btn btn-primary w-100">
-          Submit
-        </button>
-      </form>
-    </Box>
+          <button type="submit" className="btn btn-primary w-100">
+            Submit
+          </button>
+        </form>
+      </Box>
+    </VStack>
   );
 };
 
--- src/theme.ts
+++ src/theme.ts
@@ -12,21 +12,25 @@ const config: ThemeConfig = {
 // 3. extend the theme
 const theme = extendTheme({
   config,
-  // darks shades
-  // colors: {
-  //   gray: {
-  //     50: "f9f9f9",
-  //     100: "ededed",
-  //     200: "d3d3d3",
-  //     300: "b3b3b3",
-  //     400: "a0a0a0",
-  //     500: "898989",
-  //     600: "6c6c6c",
-  //     700: "202020",
-  //     800: "121212",
-  //     900: "111",
-  //   },
-  // },
+  // combined light and dark shades
+  colors: {
+    gray: {
+      50: "#f7fafc",
+      100: "#edf2f7",
+      200: "#e2e8f0",
+      300: "#cbd5e0",
+      400: "#a0aec0",
+      500: "#718096",
+      600: "#4a5568",
+      700: "#2d3748",
+      800: "#1a202c",
+      900: "#171923",
+    },
+  },
+  fonts: {
+    heading: `'Poppins', 'sans-serif'`,
+    body: `'Poppins', 'sans-serif'`,
+  },
 });
 
 export default theme;
